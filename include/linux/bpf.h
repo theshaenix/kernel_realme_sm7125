16af66b3359407e09d2047ac563e785516580098
Jakub Kicinski <jakub.kicinski@netronome.com>
2018-01-11 20:29:07 -0800

    BACKPORT: bpf: rename bpf_dev_offload -> bpf_prog_offload
    With map offload coming, we need to call program offload structure
    something less ambiguous.  Pure rename, no functional changes.
    
    Change-Id: I66dad227ab0fc56c938ce25aae700f567a564609
    Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Reviewed-by: Quentin Monnet <quentin.monnet@netronome.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -205,7 +205,7 @@ struct bpf_prog_offload_ops {
 			 int insn_idx, int prev_insn_idx);
 };
 
-struct bpf_dev_offload {
+struct bpf_prog_offload {
 	struct bpf_prog		*prog;
 	struct net_device	*netdev;
 	void			*dev_priv;
@@ -235,6 +235,7 @@ struct bpf_prog_aux {
 #endif
 	u64 load_time; /* ns since boottime */
 	char name[BPF_OBJ_NAME_LEN];
+	struct bpf_prog_offload *offload;
 	union {
 		struct work_struct work;
 		struct rcu_head	rcu;
