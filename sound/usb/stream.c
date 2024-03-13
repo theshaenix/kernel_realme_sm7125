19c0cbedeed4b582766d36685003cda20f3b1872
Johan Carlsson <johan.carlsson@teenage.engineering>
2024-03-13 09:15:09 +0100

    ALSA: usb-audio: Stop parsing channels bits when all channels are found.
    [ Upstream commit a39d51ff1f52cd0b6fe7d379ac93bd8b4237d1b7 ]
    
    If a usb audio device sets more bits than the amount of channels
    it could write outside of the map array.
    
    Signed-off-by: Johan Carlsson <johan.carlsson@teenage.engineering>
    Fixes: 04324ccc75f9 ("ALSA: usb-audio: add channel map support")
    Message-ID: <20240313081509.9801-1-johan.carlsson@teenage.engineering>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    (cherry picked from commit 7e2c1b0f6dd9abde9e60f0f9730026714468770f)
    Signed-off-by: Harshit Mogalapalli <harshit.m.mogalapalli@oracle.com>

--- sound/usb/stream.c
+++ sound/usb/stream.c
@@ -313,6 +313,8 @@ static struct snd_pcm_chmap_elem *convert_chmap(int channels, unsigned int bits,
 			for (; bits && *maps; maps++, bits >>= 1)
 				if (bits & 1)
 					chmap->map[c++] = *maps;
+				if (c == chmap->channels)
+					break;
 		} else {
 			/*
 			 * If we're missing wChannelConfig, then guess something
